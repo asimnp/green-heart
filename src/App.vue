471a40efe0bb7cba5d981058719ac45e6aa8584c
Asim Shrestha <asimshrestha608@gmail.com>
2022-03-16 20:23:54 +0545

    add bootstrap 5

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
 <template>
   <h1>Green Heart Starter App</h1>
+  <div class="alert alert-success alert-dismissible fade show" role="alert">
+    <strong>Bootstrap 5</strong> successfully added!
+    <button
+      type="button"
+      class="btn-close"
+      data-bs-dismiss="alert"
+      aria-label="Close"
+    ></button>
+  </div>
 </template>
